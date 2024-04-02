6cd02a1ad66d9795a88d1e3d0dbe4ea6bd55be02
zzang535 <zzang535@gmail.com>
2024-04-03 06:21:47 +0900

    Feat : build script added

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,19 @@
 #!/bin/bash
 
-# 색상 출력 함수 정의
+# color print method
 print_colored() {
   local COLOR=$1
   local MSG=$2
   printf "\e[${COLOR}m${MSG}\e[0m\n"
 }
 
-# 색상 코드
+# color code
 YELLOW="33"
 
-# 스크립트 실행
-print_colored $YELLOW "Starting deployment..."
-
-# .env.local 파일에서 환경변수 가져오기
-print_colored $YELLOW "Loaded the environment variables."
-if [ -f .env.local ]; then
-  export $(cat .env.local | xargs)
-fi
-
-# 프로젝트 빌드 및 내보내기
-print_colored $YELLOW "Built the project."
+# build
 npm run build
+print_colored $YELLOW "Built the project."
 
-# AWS S3에 동기화
-print_colored $YELLOW "Synchronized the AWS directory with S3."
-aws s3 sync ./build s3://$S3_BUCKET_NAME --profile=$IAM_USER
-
-# CloudFront 캐시 무효화
-print_colored $YELLOW "Invalidated the CloudFront cache."
-aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*" --no-cli-pager --profile $IAM_USER
-
+# s3 sync
+aws s3 sync ./dist s3://threejs-material.bird89.com --profile=default
 print_colored $YELLOW "Finished deployment."
